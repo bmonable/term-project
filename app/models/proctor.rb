b733f78737c562889e5372324a1d538b5254ffe6
bmon <baimon_kks@hotmail.com>
2015-05-16 08:13:15 +0000

    finish

--- app/models/proctor.rb
+++ app/models/proctor.rb
@@ -2,7 +2,7 @@ class Proctor < ActiveRecord::Base
   belongs_to :room
   attr_accessible :email, :name, :position, :tel, :room_id
 
-  validates :email, :presence => true 
+  validates :email, :presence => true , uniqueness:true
   validates :position, :presence => true
   validates :tel, :presence => true
   validates :name, :presence => true , uniqueness:true
